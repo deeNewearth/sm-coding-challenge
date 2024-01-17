228b1e619774bb749ee4cef334f2f7ff759c559e
dee acharjya <deeAcharjya@gamestop.com>
2024-01-16 22:41:21 -0500

    Exception handling and final comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using sm_coding_challenge.Models;
 using sm_coding_challenge.Services.DataProvider;
 
@@ -13,10 +15,17 @@ namespace sm_coding_challenge.Controllers
     public class HomeController : Controller
     {
 
-        private IDataProvider _dataProvider;
-        public HomeController(IDataProvider dataProvider)
+        readonly IDataProvider _dataProvider;
+        readonly ILogger _logger;
+
+
+        public HomeController(
+            IDataProvider dataProvider,
+            ILogger<HomeController> logger
+            )
         {
             _dataProvider = dataProvider ?? throw new ArgumentNullException(nameof(dataProvider));
+            _logger = logger;
         }
 
         public IActionResult Index()
@@ -24,17 +33,50 @@ namespace sm_coding_challenge.Controllers
             return View();
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Player(string id)
+        ObjectResult fromError(Exception ex)
         {
-            return Json(await _dataProvider.GetPlayerById(id));
+
+            var errorId = Guid.NewGuid().ToString();
+
+            _logger.LogError(ex, $"error id {errorId}" );
+
+            if (ex is FileNotFoundException)
+            {
+                return StatusCode((int)HttpStatusCode.NotFound,new { error = "not found", errorId});
+            }
+
+            return StatusCode((int)HttpStatusCode.InternalServerError, new { error = "generic error", errorId });
         }
 
         [HttpGet]
-        public async Task<IActionResult> Players(string id)
+        public async Task<IActionResult> Player(string id)
         {
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                    throw new ArgumentNullException(nameof(id));
+
+
+                return Json(await _dataProvider.GetPlayerById(id));
+            }
+            catch(Exception ex)
+            {
+                
+                return fromError(ex);
+            }
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Players(string ids)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(ids))
+                return Json(Array.Empty<string>());
+
             // dee : We should remove duplicated Ids fetched
-            var idList = id.Split(',').Distinct();
+            var idList = ids.Split(',').Distinct();
 
             var playersMap = await _dataProvider.GetPlayerMap();
 
@@ -45,27 +87,45 @@ namespace sm_coding_challenge.Controllers
                 .ToArray();
 
             return Json(returnList);
+            }
+            catch (Exception ex)
+            {
+                
+                return fromError(ex);
+            }
         }
 
         [HttpGet]
-        public async Task<IActionResult> LatestPlayers(string id)
+        public async Task<IActionResult> LatestPlayers(string ids)
         {
-            var playersMap = await _dataProvider.GetPlayerMap();
-
-            if (playersMap.TryGetValue(id, out var playerAndPos))
+            try
             {
-                var ret = (from p in playerAndPos
-                          group p by p.Position into g
-                          select new { position = g.Key, players = g.ToArray() })
-                          .ToDictionary(k=>k.position, v=>v.players)
+                if (string.IsNullOrEmpty(ids))
+                    return Json(Array.Empty<string>());
+
+                var playersMap = await _dataProvider.GetPlayerMap();
+
+                var idList = ids.Split(',').Distinct();
+
+                var returnList = idList.Select(anId =>
+                    playersMap.ContainsKey(anId) ? playersMap[anId] : null)
+                    .Where(r => null != r)
+                    .ToArray();
+
+                var ret = (from p in returnList.SelectMany(p => p)
+                           group p by p.Position into g
+                           select new { position = g.Key, players = g.ToArray() })
+                          .ToDictionary(k => k.position, v => v.players.Select(p => p.Player))
                           ;
 
                 return Json(ret);
             }
-            else
+            catch (Exception ex)
             {
-                throw new FileNotFoundException();
+                
+                return fromError(ex);
             }
+
         }
 
         public IActionResult Error()
--- README.md
+++ README.md
@@ -18,19 +18,39 @@ Currently this project has an 'IDataProvider' interface that has a a single meth
 ## Requirements
 
 * All urls on the home page should return without error
+ ==Done==
+
 * Update the DataProvider interface and implementation to use async/await
+==Done==
+
 * Refactor the DataProvider implementation as you see fit.  Comment on the changes you made.
+==Comments in the code, Data is cached and fetched when cache expires==
+
 * Add missing player attributes to the fetch so all data from the data provider is returned to the front-end
+==Done==
+
 * Duplicates should be removed from the existing GetPlayers result(s)
+==I believe the duplicates we are refering to duplicate IDs being passed as input. That is fixed==
+
 * Implement the "LatestPlayers" method to return a structure similar to: https://gist.githubusercontent.com/RichardD012/a81e0d1730555bc0d8856d1be980c803/raw/3fe73fafadf7e5b699f056e55396282ff45a124b/output.json.  Note, the concept of latest player is simply from a client's "needs".  Latest is in reference to the latest fetch of the above basic.json endpoint.  For this exercise the results will always be the same.
+==Done==
+
 * All responses should be performant.  None of these should take longer than a few miliseconds.  There are multiple solutions/avenues to this but think about the frequency that you fetch the data and different ways to mitigate iterating over too much data or making multiple requests.
+==Best would be to fetch this data on a worker thread and populate a model in memory. For this coding challange, data is fetched on the first call and saved in cache==
+
 * If you remove/change/invalidate the url from the DataProvider fetch method, the system should still "work" (fail gracefully - up to you on your definition of work).
+==Added Exception handling, if endpoint is not available we will get JSON back with status code set==
 
 ### Considerations in Design
 
 * What would happen if the remote endpoint goes down?  What are some ways to mitigate this that are transparent to the user?
+==Depends on if Stale data is Dangerous. if Not we can keep serving data from Cache till the remote endpoint comes back==
+
 * What are some ways to prevent multiple calls to the data endpoint?
+==Since we are caching the chances of multiple calls is rare, but we can prevent that using a mutex, if we really need to==
+
 * This data set is not updated very frequently (once a week), what are some ways we could take advantage of this in our system?
+==we are using a distributed Cache==
 
 ## Notes
 
